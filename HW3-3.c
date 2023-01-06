07385d2fd0bfd55af5b2e93797a0135011db88cd
Mayochiki03 <aphisorn2003@gmail.com>
2023-01-06 18:36:34 +0700

    HW3-3
    update

--- HW3-3.c
+++ HW3-3.c
@@ -1,26 +1,29 @@
-#include <stdio.h>//0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25
-char str[2][26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-                   'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', };              
-int input,s;            
+#include <stdio.h>          
+int input,s,i=0;   
+char data[11];         
 int main()
 {
-    print();
+    printf("** Main Menu for 10 data **\n1. Input Data\n2. Search Data\n3. Edit Data\n4. Show All Data\n5. Show Max Data\n6. Show Min Data\n7. Exit\nSelect Menu : ");
     scanf("%d", &input);
     s=scase(input);
-}
-
-print()
-{
-    printf("** Main Menu for 10 data **\n1. Input Data\n2. Search Data\n3. Edit Data\n4. Show All Data\n5. Show Max Data\n6. Show Min Data\n7. Exit\nSelect Menu : ");
 }
 scase()
 {                                       
     switch (input)
     {
     case 1:
+        printf("*** Input Data ***\n");
+        line();
+        do
+        {
+            printf("Enter Number[%d] : ",i);
+            scanf("%d",&data);
+            i++;
+        } while (i!=10);
         break;
     case 2:
-        /* code */
+    printf("*** search Data ***\n");
+        printf("");
         break;
     case 3:
         /* code */
@@ -41,4 +44,8 @@ scase()
        // printf("Hello wold");
         break;
     }
+}
+line(){
+ printf("===================\n");
+
 }
