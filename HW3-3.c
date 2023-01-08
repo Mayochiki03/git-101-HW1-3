ebd9b4b02b68424f48a12d30ce5ed4cb519d30aa
Mayochiki03 <aphisorn2003@gmail.com>
2023-01-09 02:28:25 +0700

    Update HW3-3.c

--- HW3-3.c
+++ HW3-3.c
@@ -1,47 +1,110 @@
 #include <stdio.h>
-int input, s, i = 0, h;
-char data[11];
+int input = 0, s, i = 0, h, b, c, d;
+int data[11], search;
+
 int main()
 {
-    headprint();
-    scanf("%d", &input);
-    s = scase(input);
+    do
+    {
+        h = ifs();
+        headprint();
+        scanf("%d", &input);
+        system("cls");
+    } while (input != 0);
+    b = ifs(input);
+}
+ifs()
+{
+    switch (input)
+    {
+    case 1:
+        case1();
+        printf("Press any key to menu :\n");
+        getch();
+        system("cls");
+        break;
+    case 2:
+        case2();
+        printf("Press any key to menu :\n");
+        getch();
+        system("cls");
+        break;
+    case 3:
+        case3();
+        printf("Press any key to menu :\n");
+        getch();
+        system("cls");
+        break;
+    case 4:
+        case4();
+        printf("Press any key to menu :\n");
+        getch();
+        system("cls");
+        break;
+    case 5:
+
+        break;
+    case 6:
+
+        break;
+    case 7:
+
+        break;
+    }
+    return (h);
 }
 headprint()
 {
     printf("** Main Menu for 10 data **\n1. Input Data\n2. Search Data\n3. Edit Data\n4. Show All Data\n5. Show Max Data\n6. Show Min Data\n7. Exit\nSelect Menu : ");
 }
-scase()
-{
-    switch (input)
-    {
-    case 1:
-
-        break;
-    case 2:
-
-        break;
-    case 3:
-        /* code */
-        break;
-    case 4:
-        /* code */
-        break;
-    case 5:
-        /* code */
-        break;
-    case 6:
-        /* code */
-        break;
-    case 7:
-        /* code */
-        break;
-    default:
-        // printf("Hello wold");
-        break;
-    }
-}
 line()
 {
     printf("===================\n");
+}
+case1()
+{
+    printf("** Input Data **\n");
+    line();
+    for (int i = 0; i <= 5; i++)
+    {
+        printf("Enter Number [%d]:", i);
+        scanf("%d", &data[i]);
+    }
+}
+case2()
+{
+    i = -1;
+    printf("** Search Data **\n");
+    line();
+    printf("Enter Data :");
+    scanf("%d", &search);
+    do
+    {
+        i++;
+    } while (search != data[i]);
+    printf("Position Data >> [%d]\n", i);
+}
+case3()
+{
+    printf("** Edit Data **\n");
+    line();
+    printf("Enter Position :");
+    scanf("%d", &search);
+    do
+    {
+        i++;
+        d = i;
+    } while (search != i);
+    printf("Enter Data :");
+    scanf("%d", &data[d]);
+}
+case4()
+{
+}
+case5()
+{
+}
+case6()
+{
+    input = 0;
 }
